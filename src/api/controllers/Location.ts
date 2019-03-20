b4798f8f756c0792134a5f19e3850a8866ee4c75
johadi <jim.hadi10@gmail.com>
2019-03-20 01:17:10 +0100

    implement the route and controller for location

--- src/api/controllers/Location.ts
+++ src/api/controllers/Location.ts
@@ -1,8 +1,83 @@
 import * as express from 'express';
-import { handleSuccess } from "../helpers/helpers";
+import db = require('../../database/models');
+import {handleError, handleSuccess} from "../helpers/helpers";
+
+
 
 export class Location {
   static add(req: express.Request, res: express.Response) {
-    return handleSuccess({code: 201, data: 'Success'}, res);
+
+    return db.Location.findOrCreate({where: req.body})
+      .then(([location, created]: any[]) => {
+        if (!created) {
+          return Promise.reject({code: 409, message: 'This location already exists'});
+        }
+
+        return handleSuccess({code: 201, data: location}, res);
+      })
+      .catch((err: any) => handleError(err, res));
+  }
+
+  static get(req: express.Request, res: express.Response) {
+    if (!req.params.name) {
+      return Promise.reject({code: 400, message: 'Parameter name is required'});
+    }
+
+    return db.Location.findOne({where: { name: req.params.name }})
+      .then((location: any) => {
+        if (!location) {
+          return Promise.reject({code: 404, message: 'Location not found'});
+        }
+
+        return handleSuccess({code: 200, data: location}, res);
+      })
+      .catch((err: any) => handleError(err, res));
+  }
+
+  static update(req: express.Request, res: express.Response) {
+    if (!req.params.name) {
+      return Promise.reject({code: 400, message: 'Parameter name is required'});
+    }
+
+    return db.Location.findOne({where: { name: req.params.name }})
+      .then((location: any) => {
+        if (!location) {
+          return Promise.reject({code: 404, message: 'Location not found'});
+        }
+
+        return location.update(req.body);
+      })
+      .then((updatedLocation: any) => handleSuccess({code: 200, data: updatedLocation}, res))
+      .catch((err: any) => handleError(err, res));
+  }
+
+  static getAll(req: express.Request, res: express.Response) {
+
+    return db.Location.findAll()
+      .then((locations: any[]) => {
+        if (locations.length === 0) {
+          return Promise.reject({code: 404, message: 'No location found'});
+        }
+
+        return handleSuccess({code: 200, data: locations}, res);
+      })
+      .catch((err: any) => handleError(err, res));
+  }
+
+  static delete(req: express.Request, res: express.Response) {
+    if (!req.params.name) {
+      return Promise.reject({code: 400, message: 'Parameter name is required'});
+    }
+
+    return db.Location.findOne({where: { name: req.params.name }})
+      .then((location: any) => {
+        if (!location) {
+          return Promise.reject({code: 404, message: 'Location not found'});
+        }
+
+        return location.destroy();
+      })
+      .then(() => handleSuccess({code: 200, data: 'Location deleted'}, res))
+      .catch((err: any) => handleError(err, res));
   }
 }
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,11 +1,16 @@
 import * as express from 'express';
 import { Location } from '../controllers';
+import { validateCreateLocation } from '../middlewares';
 const router = express.Router();
 
 router.get('/', function(req: express.Request, res: express.Response) {
   return res.json('Welcome to Population management system API');
 });
 
-router.post('/api/location', Location.add);
+router.post('/api/location', validateCreateLocation, Location.add);
+router.get('/api/location/:name', Location.get);
+router.get('/api/locations', Location.getAll);
+router.patch('/api/location/:name', Location.update);
+router.delete('/api/location/:name', Location.delete);
 
 export default router;
